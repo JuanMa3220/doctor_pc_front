e756a24d2891c7d9c87da8deaaefffff59ac0d42
Juan Manuel Hurtado <juanma0223@hotmail.com>
2023-05-23 19:33:13 -0500

    Se organiza la pagina de admin que esta en proceso

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,28 +1,41 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header reveal elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
-        <q-toolbar-title>
-          Doctor PC
-        </q-toolbar-title>
-        <q-btn flat icon-right="logout" to="/"/>
+        <q-toolbar-title> Doctor PC </q-toolbar-title>
+        <q-btn flat icon-right="logout" to="/" />
       </q-toolbar>
 
       <q-tabs align="left">
         <q-route-tab to="/dispositivos" label="Dispositivos" />
         <q-route-tab to="/facturas" label="Facturas" />
-        <q-route-tab to="/admin" label="Administración" />
+        <q-route-tab label="Administración" @click="showAdminWarning = true" />
       </q-tabs>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
+
+  <q-dialog v-model="showAdminWarning">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Alert</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Esta sección se encuentra en construcción.
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="OK" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup>
-
+import { ref } from "vue";
+let showAdminWarning = ref(false);
 </script>
--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,42 +1,5 @@
 <template>
-  <div
-    class="q-pa-md text-center q-mx-auto"
-    style="max-width: 40rem"
-  >
+  <div class="q-pa-md text-center q-mx-auto" style="max-width: 40rem">
     Admnin works!
   </div>
 </template>
-
-<script setup>
-import { ref } from "vue";
-import { api } from "boot/axios";
-import { useRouter } from "vue-router";
-
-let document = ref(null);
-let name = ref(null);
-let phone = ref(null);
-let password = ref(null);
-let confirmPassword = ref(null);
-
-const router = useRouter();
-
-async function onSubmit() {
-  let response = await api.post("/register", {
-    document: document.value,
-    name: name.value,
-    phone: phone.value,
-    password: password.value,
-  });
-
-  router.push("/home");
-}
-
-function onReset() {
-  document.value = null;
-  name.value = null;
-  phone.value = null;
-  confirmPassword.value = null;
-  password.value = null;
-}
-</script>
-
